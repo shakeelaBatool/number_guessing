8d9c4620f1cfc618aedf91dfd5c8fd244936e504
Shakeela Sadpara <shakeelasadpara@gmail.com>
2025-08-17 22:46:02 -0700

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,24 @@
+import random
+
+def number_guess():
+    com_guess = random.randint(1, 100)
+    print("Computer has chosen a number between 1 and 100.")
+    
+    while True:
+        try:
+            user_guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+        
+        if user_guess < com_guess:
+            print("Low guess, try again!")
+        elif user_guess > com_guess:
+            print("High guess, try again!")
+        else:
+            print("Congratulations! Your guess matches the number.")
+            break
+        return user_guess
+
+# Start the game
+number_guess()
